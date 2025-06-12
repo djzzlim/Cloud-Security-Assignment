39be0e8829aabfdd3d15157992da50b685d8bd19
Lim Kar Joon <djzzlim@gmail.com>
2025-06-12 09:13:31 +0800

    Updated models.py

--- config.py
+++ config.py
@@ -2,7 +2,10 @@ import os
 
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = f''
+    SQLALCHEMY_DATABASE_URI = (
+        'mssql+pyodbc://sa:Pa$$w0rd@localhost:1433/BankingAppDB'
+        '?driver=ODBC+Driver+17+for+SQL+Server&trustServerCertificate=yes'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
     SECURITY_PASSWORD_SALT = 'secret-salt'
